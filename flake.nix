2664acd41c8ec95792f3ee912da9f4373a560236
lucaaaaum <lucasrenato.fernandesdasilva@gmail.com>
2025-11-09 11:00:40 -0300

    Creating flake files

---
+++ flake.nix
@@ -0,0 +1,60 @@
+{
+  description = "supalink";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixpkgs-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      version = "0.0.1";
+      supportedSystems = [
+        "x86_64-linux"
+        "x86_64-darwin"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+    in
+    {
+      packages = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgsFor.${system};
+        in
+        {
+          supalink = pkgs.buildGoModule {
+            pname = "supalink";
+            inherit version;
+            src = ./.;
+            vendorHash = null;
+            nativeBuildInput = with pkgs; [
+              go
+              gopls
+              gotools
+              go-tools
+            ];
+          };
+        }
+      );
+
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgsFor.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              go
+              gopls
+              gotools
+              go-tools
+            ];
+          };
+        }
+      );
+    };
+}
