76aeca184905946ee4d88a500d02cc8d774fff02
lucaaaaum <lucasrenato.fernandesdasilva@gmail.com>
2025-11-15 23:51:47 -0300

    Breaking. Up. More. Code.

--- main.go
+++ main.go
@@ -2,11 +2,7 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"path/filepath"
-	"strings"
 	"supalink/src/filesystem"
-	"supalink/src/output"
 	"supalink/src/settings"
 	"supalink/src/utils"
 
@@ -36,47 +32,12 @@ var rootCmd = &cobra.Command{
 			return nil
 		}
 
-		createSymlinks(matchingPathsAndDestinations, settings)
+		filesystem.PrintSymlinks(matchingPathsAndDestinations, settings)
+
+		filesystem.CreateSymlinks(matchingPathsAndDestinations, settings)
 
 		return nil
 	},
-}
-
-func createSymlinks(matchingPathsAndDestinations map[string]string, settings settings.Settings) {
-	printSymlinks(matchingPathsAndDestinations, settings)
-
-	if settings.DryRun {
-		fmt.Println("Dry run enabled, no symlinks will be created.")
-		return
-	}
-
-	if settings.Confirm {
-		var response string
-		fmt.Print("Are you sure you want to create these symlinks? (y/n): ")
-		fmt.Scanln(&response)
-		if strings.ToLower(response) != "y" {
-			fmt.Println("Operation cancelled by user.")
-			return
-		}
-	}
-
-	for source, destination := range matchingPathsAndDestinations {
-		os.MkdirAll(filepath.Dir(destination), os.ModePerm)
-		err := os.Symlink(source, destination)
-		if err != nil {
-			fmt.Printf("Failed to create symlink: %s -> %s. Error: %v\n", source, destination, err)
-		} else {
-		}
-	}
-}
-
-func printSymlinks(matchingPathsAndDestinations map[string]string, settings settings.Settings) {
-	switch settings.Format {
-	case output.TreeFormat:
-		output.PrintAsTree(matchingPathsAndDestinations)
-	case output.TableFormat:
-		output.PrintAsTable(matchingPathsAndDestinations)
-	}
 }
 
 func main() {
